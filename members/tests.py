a16072c26aa0424635bf30ebd0628affc9a2594f
Bernard Siebens <bernard@siebens.org>
2024-07-29 23:55:40 +0200

    Added unit test for creation of a new Member
    Signed-off-by: Bernard Siebens <bernard@siebens.org>

--- members/tests.py
+++ members/tests.py
@@ -1,3 +1,16 @@
+import re
+
+from django.contrib.auth.hashers import check_password
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Member
+
+
+class MemberTest(TestCase):
+    def test_member_creation(self):
+        member = Member.obejcts.create(first_name="First Name", last_name="Last Name", email="firstName.lastName@test.com")
+
+        self.assertIsNotNone(member.user)
+
+        password = re.match(r"^Initial password: ([a-zA-Z0-9]+)$", member.notes).groups()[0]
+        self.assertTrue(check_password(password, member.user.password))
