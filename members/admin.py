eb218cafd9043e61f82a804c2f351815e9234953
Bernard Siebens <bernard@siebens.org>
2024-07-30 00:04:35 +0200

    Initial Member model, activating admin without modification of display and fields
    Signed-off-by: Bernard Siebens <bernard@siebens.org>

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Member, Family
+
+admin.site.register(Member)
+admin.site.register(Family)
--- members/models.py
+++ members/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
 
-class Member(models.Model): ...
+class Member(models.Model):
+    """User profile for a given member."""
+
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, verbose_name=_("user"))
 
 
 class Family(models.Model): ...
