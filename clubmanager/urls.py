79c4e636ba4b630f4eee0d13ea09446916b53ae0
Bernard Siebens <bernard@siebens.org>
2024-07-31 23:10:02 +0200

    Running a quick check that all is working well
    Signed-off-by: Bernard Siebens <bernard@siebens.org>

--- clubmanager/urls.py
+++ clubmanager/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from members import views as members_views
+
 urlpatterns = [
+    path("test/", members_views.index),
     path("admin/", admin.site.urls),
 ]
