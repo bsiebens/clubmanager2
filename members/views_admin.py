f50379962df7bb6d10c72d2fa06b07eab1ea99eb
Bernard Siebens <bernard@siebens.org>
2024-08-04 15:09:45 +0200

    Removing translations for now and unused imports
    Signed-off-by: Bernard Siebens <bernard@siebens.org>

--- members/views_admin.py
+++ members/views_admin.py
@@ -1,11 +1,6 @@
-from typing import Any
-from django.db.models.query import QuerySet
-from .models import Member
-from django.views.generic.list import ListView
-from django.views import View
-from django.shortcuts import render
-from .filters import MemberFilter
 from django_filters.views import FilterView
+
+from .filters import MemberFilter
 
 
 class MemberList(FilterView):
