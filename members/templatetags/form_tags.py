685b281301e5385299bc2081ac243bff6d3dea73
Bernard Siebens <bernard@siebens.org>
2024-08-05 14:55:25 +0200

    Mobile display also working for the listview
    Signed-off-by: Bernard Siebens <bernard@siebens.org>

--- members/templatetags/form_tags.py
+++ members/templatetags/form_tags.py
@@ -5,7 +5,13 @@ register = template.Library()
 
 
 @register.inclusion_tag("templatetags/display_field.html")
-def display_field(field: BoundField, alternate_label: str | None = None, display_label: bool = True, display_placeholder: bool = True):
+def display_field(
+    field: BoundField,
+    alternate_label: str | None = None,
+    display_label: bool = True,
+    display_placeholder: bool = True,
+    display_size: str = "default",
+):
     """
     Print HTML for a field.
     Optionally, a label can be supplied that overrides the default label generated for the form.
@@ -16,11 +22,17 @@ def display_field(field: BoundField, alternate_label: str | None = None, display
     if alternate_label is not None:
         field.label = alternate_label
 
-    print(field.widget_type)
+    input_classes = ""
+    match display_size:
+        case "xs":
+            input_classes = "input-xs"
+        case _:
+            input_classes = ""
 
     return {
         "field": field,
         "type": field.widget_type,
         "display_label": display_label,
         "display_placeholder": display_placeholder,
+        "input_classes": input_classes,
     }
