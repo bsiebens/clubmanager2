9fa0465e364b2fde0023da9097e3c23675a5a811
Bernard Siebens <bernard@siebens.org>
2024-08-05 22:02:29 +0200

    Deleting members now works
    Signed-off-by: Bernard Siebens <bernard@siebens.org>

--- members/urls_admin.py
+++ members/urls_admin.py
@@ -4,5 +4,6 @@ from . import views_admin
 
 app_name = "members"
 urlpatterns = [
-    path("", views_admin.MemberList.as_view(), name="index"),
+    path("", views_admin.MemberListView.as_view(), name="index"),
+    path("delete/<int:pk>/", views_admin.MemberDeleteView.as_view(), name="delete"),
 ]
--- members/views_admin.py
+++ members/views_admin.py
@@ -1,8 +1,19 @@
 from django_filters.views import FilterView
+from django.views.generic.edit import DeleteView
 
 from .filters import MemberFilter
+from .models import Member
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext_lazy as _
 
 
-class MemberList(FilterView):
+class MemberListView(FilterView):
     filterset_class = MemberFilter
     paginate_by = 50
+
+
+class MemberDeleteView(SuccessMessageMixin, DeleteView):
+    model = Member
+    success_url = reverse_lazy("clubmanager_admin:members:index")
+    success_message = _("Member was succesfully deleted")
