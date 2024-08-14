be6c11f5c4150cc8ceb2a05af81d4ce75c7bbbd0
Bernard Siebens <bernard@siebens.org>
2024-08-14 13:40:28 +0200

    Admin updated
    Signed-off-by: Bernard Siebens <bernard@siebens.org>

--- news/admin.py
+++ news/admin.py
@@ -4,11 +4,22 @@ from rules.contrib.admin import ObjectPermissionsModelAdmin
 from .models import NewsItem, Picture
 
 
+class PictureInlineAdmin(admin.TabularInline):
+    model = Picture
+    extra = 0
+
+
 @admin.register(NewsItem)
 class NewsAdmin(ObjectPermissionsModelAdmin):
-    list_display = ["title", "slug"]
-
-
-@admin.register(Picture)
-class PictureAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["title", "status", "type", "publish_on", "modified"]
+    date_hierarchy = "publish_on"
+    list_filter = ["status", "type", "modified"]
+    search_fields = ["title", "text"]
+    inlines = [PictureInlineAdmin]
+    raw_id_fields = ["author"]
+    fieldsets = [
+        ["GENERAL INFORMATION", {"fields": ["title", "text"]}],
+        ["METADATA", {"fields": ["author", ("status", "type"), "publish_on"]}],
+        ["OTHER", {"fields": ["slug"]}],
+    ]
+    readonly_fields = ["slug"]
