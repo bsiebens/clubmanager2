38bb9b3797a9c91c8680994f0b3818f9bd2166c8
EC2 Default User <ec2-user@ip-172-26-3-120.eu-central-1.compute.internal>
2024-09-04 13:53:54 +0000

    Updates to run on AWS Linux 2023

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,2 @@
 cd /home/ubuntu/clubmanager
-/home/ubuntu/.cache/pypoetry/virtualenvs/clubmanager-wvKJ8zXl-py3.10/bin/gunicorn clubmanager.wsgi
+/home/ec2-user/.cache/pypoetry/virtualenvs/clubmanager-wnM1rr7f-py3.11/bin/gunicorn clubmanager.wsgi
