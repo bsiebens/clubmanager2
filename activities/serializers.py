0c7aef1f31a09c00a61718c0d802512d88325c2a
Bernard Siebens <bernard@siebens.org>
2024-09-29 00:08:27 +0200

    Added a passed attribute to the API

--- activities/serializers.py
+++ activities/serializers.py
@@ -1,6 +1,9 @@
-from .models import Opponent, Game
-from teams.serializers import TeamNameSerializer
+from django.utils import timezone
 from rest_framework import serializers
+
+from teams.serializers import TeamNameSerializer
+
+from .models import Game, Opponent
 
 
 class OpponentNameSerializer(serializers.ModelSerializer):
@@ -18,10 +21,14 @@ class GameSerializer(serializers.ModelSerializer):
     team = TeamNameSerializer()
     opponent = OpponentNameSerializer()
     game_type = serializers.SerializerMethodField()
+    passed = serializers.SerializerMethodField()
 
     class Meta:
         model = Game
-        fields = ["id", "team", "opponent", "date", "location", "live", "score_team", "score_opponent", "game_type"]
+        fields = ["id", "team", "opponent", "date", "location", "live", "score_team", "score_opponent", "game_type", "passed"]
 
     def get_game_type(self, obj: Game) -> str:
         return obj.game_type.name
+
+    def get_passed(self, obj: Game) -> bool:
+        return obj.date <= timezone.now()
